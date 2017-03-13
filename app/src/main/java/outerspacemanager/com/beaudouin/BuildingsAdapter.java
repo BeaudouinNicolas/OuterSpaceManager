280ef745cae86d3c89113839ce07da0558f74a5c
Nicolas Beaudouin <n.beaudouin@outlook.fr>
2017-03-13 17:04:45 +0100

    Update DataAdapter to buildings list view

--- app/src/main/java/outerspacemanager/com/beaudouin/BuildingsAdapter.java
+++ app/src/main/java/outerspacemanager/com/beaudouin/BuildingsAdapter.java
@@ -1,20 +1,29 @@
 package outerspacemanager.com.beaudouin;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.ArrayList;
-import java.util.List;
 
 import outerspacemanager.com.beaudouin.models.Building;
-import outerspacemanager.com.beaudouin.models.Buildings;
+import outerspacemanager.com.beaudouin.models.User;
+import outerspacemanager.com.beaudouin.services.OSMService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by nico on 07/03/17.
@@ -24,11 +33,30 @@ public class BuildingsAdapter extends ArrayAdapter<Building> {
 
     private final Context context;
     private final ArrayList<Building> objects;
+    private Float userGas;
+    private Float userMinerals;
+
+    private OSMService osmService = OSMService.retrofit.create(OSMService.class);
+    private static final String PREFS_NAME = "PreferencesFile";
 
     public BuildingsAdapter(@NonNull Context context, @NonNull ArrayList<Building> objects) {
         super(context, R.layout.building_adapter, objects);
         this.context = context;
         this.objects = objects;
+
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        final Call<User> currentUser = osmService.getCurrentUser(settings.getString("userToken", ""));
+        currentUser.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                userGas = response.body().getGas();
+                userMinerals = response.body().getMinerals();
+            }
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Log.e("An error occurred : ", t.getMessage());
+            }
+        });
     }
 
     @NonNull
@@ -37,29 +65,51 @@ public class BuildingsAdapter extends ArrayAdapter<Building> {
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.building_adapter, parent, false);
 
+        setRowAdapter(position, rowView);
+
+        return rowView;
+    }
+
+    public void setRowAdapter(Integer position, View rowView) {
         TextView buildingName = (TextView)rowView.findViewById(R.id.buildingName);
         TextView mineralsCost = (TextView)rowView.findViewById(R.id.mineralCost);
         TextView gasCost = (TextView)rowView.findViewById(R.id.gasCost);
         TextView buildingEffect = (TextView)rowView.findViewById(R.id.buildingEffect);
         TextView timeToBuild = (TextView)rowView.findViewById(R.id.timeToBuild);
+        TextView buildingLevel = (TextView)rowView.findViewById(R.id.buldingLevel);
+        ImageView imageBuilding = (ImageView)rowView.findViewById(R.id.buildingImage);
+        Button levelUp = (Button)rowView.findViewById(R.id.levelUp);
 
         Building b = objects.get(position);
         buildingName.setText(b.getName());
-        buildingEffect.setText("Effet : " + b.getEffect());
+        buildingEffect.setText(context.getString(R.string.buildingEffect, b.getEffect()));
+        buildingLevel.setText(b.getLevel().toString());
+        Glide
+                .with(context)
+                .load(b.getImageUrl())
+                .into(imageBuilding);
+
+        Float mineralsNeed;
+        Float gasNeed;
+        Integer timeNeed;
+
         if(b.getLevel() > 0) {
-            Integer mineralsNeed = b.getMineralCostByLevel() * b.getLevel();
-            Integer gasNeed = b.getGasCostByLevel() * b.getLevel();
-            Integer timeNeed = b.getTimeToBuildByLevel() * b.getLevel();
-
-            mineralsCost.setText(mineralsNeed.toString());
-            gasCost.setText(gasNeed.toString());
-            timeToBuild.setText("Durée : " + timeNeed.toString());
+            mineralsNeed = b.getMineralCostByLevel() * b.getLevel();
+            gasNeed = b.getGasCostByLevel() * b.getLevel();
+            timeNeed = b.getTimeToBuildByLevel() * b.getLevel();
         } else {
-            mineralsCost.setText(b.getGasCostLevel0().toString());
-            gasCost.setText(b.getGasCostLevel0().toString());
-            timeToBuild.setText("Durée : " + b.getTimeToBuildLevel0().toString());
+            mineralsNeed = b.getGasCostLevel0();
+            gasNeed = b.getGasCostLevel0();
+            timeNeed = b.getTimeToBuildLevel0();
         }
+        mineralsCost.setText(context.getString(R.string.mineralsCost, mineralsNeed));
+        gasCost.setText(context.getString(R.string.gasCost, gasNeed));
+        timeToBuild.setText(context.getString(R.string.timeToBuild, timeNeed));
 
-        return rowView;
+        /*if(mineralsNeed > userMinerals && gasNeed > userGas) {
+            levelUp.setCursorVisible(false);
+        } else {
+            levelUp.setCursorVisible(true);
+        }*/
     }
 }
--- app/src/main/java/outerspacemanager/com/beaudouin/models/Building.java
+++ app/src/main/java/outerspacemanager/com/beaudouin/models/Building.java
@@ -5,15 +5,16 @@ package outerspacemanager.com.beaudouin.models;
  */
 
 public class Building {
-    private Integer amountOfEffectByLevel;
-    private Integer amountOfEffectLevel0;
+    private Integer id;
+    private Float amountOfEffectByLevel;
+    private Float amountOfEffectLevel0;
     private Boolean building;
     private String effect;
-    private Integer gasCostByLevel;
-    private Integer gasCostLevel0;
+    private Float gasCostByLevel;
+    private Float gasCostLevel0;
     private Integer level;
-    private Integer mineralCostByLevel;
-    private Integer mineralCostLevel0;
+    private Float mineralCostByLevel;
+    private Float mineralCostLevel0;
     private String name;
     private Integer timeToBuildByLevel;
     private Integer timeToBuildLevel0;
@@ -21,29 +22,30 @@ public class Building {
 
     public Building() { }
 
-    public Integer getAmountOfEffectByLevel() { return this.amountOfEffectByLevel; }
-    public Integer getAmountOfEffectLevel0() { return this.amountOfEffectLevel0; }
+    public Integer getId() { return this.id; }
+    public Float getAmountOfEffectByLevel() { return this.amountOfEffectByLevel; }
+    public Float getAmountOfEffectLevel0() { return this.amountOfEffectLevel0; }
     public Boolean getBuilding() { return this.building; }
     public String getEffect() { return this.effect; }
-    public Integer getGasCostByLevel() { return this.gasCostByLevel; }
-    public Integer getGasCostLevel0() { return this.gasCostLevel0; }
+    public Float getGasCostByLevel() { return this.gasCostByLevel; }
+    public Float getGasCostLevel0() { return this.gasCostLevel0; }
     public Integer getLevel() { return this.level; }
-    public Integer getMineralCostByLevel() { return this.gasCostByLevel; }
-    public Integer getMineralCostLevel0() { return this.gasCostLevel0; }
+    public Float getMineralCostByLevel() { return this.gasCostByLevel; }
+    public Float getMineralCostLevel0() { return this.gasCostLevel0; }
     public String getName() { return this.name; }
     public Integer getTimeToBuildByLevel() { return this.timeToBuildByLevel; }
     public Integer getTimeToBuildLevel0() { return this.timeToBuildLevel0; }
     public String getImageUrl() { return this.imageUrl; }
 
-    public void setAmountOfEffectByLevel(Integer amountOfLevel) { this.amountOfEffectByLevel = amountOfLevel; }
-    public void setAmountOfEffectLevel0(Integer amountOf0) { this.amountOfEffectLevel0 = amountOf0; }
+    public void setAmountOfEffectByLevel(Float amountOfLevel) { this.amountOfEffectByLevel = amountOfLevel; }
+    public void setAmountOfEffectLevel0(Float amountOf0) { this.amountOfEffectLevel0 = amountOf0; }
     public void setBuilding(Boolean building) { this.building = building; }
     public void setEffect(String effect) { this.effect = effect; }
-    public void setGasCostByLevel(Integer gasCostLevel) { this.gasCostByLevel = gasCostLevel; }
-    public void setGasCostLevel0(Integer gasCost0) { this.gasCostLevel0 = gasCost0; }
+    public void setGasCostByLevel(Float gasCostLevel) { this.gasCostByLevel = gasCostLevel; }
+    public void setGasCostLevel0(Float gasCost0) { this.gasCostLevel0 = gasCost0; }
     public void setLevel(Integer level) { this.level = level; }
-    public void setMineralCostByLevel(Integer mineralCostLevel) { this.mineralCostByLevel = mineralCostLevel; }
-    public void setMineralCostLevel0(Integer mineralCost0) { this.mineralCostLevel0 = mineralCost0; }
+    public void setMineralCostByLevel(Float mineralCostLevel) { this.mineralCostByLevel = mineralCostLevel; }
+    public void setMineralCostLevel0(Float mineralCost0) { this.mineralCostLevel0 = mineralCost0; }
     public void setName(String name) { this.name = name; }
     public void setTimeToBuildByLevel(Integer timeBuildLevel) { this.timeToBuildByLevel = timeBuildLevel; }
     public void setTimeToBuildLevel0(Integer timeBuild0) { this.timeToBuildLevel0 = timeBuild0; }
